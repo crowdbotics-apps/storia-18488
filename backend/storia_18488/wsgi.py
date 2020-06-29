e61981e069ae41cae090f9c029b51074acabfa03
Crowdbotics <team@crowdbotics.com>
2020-06-29 17:56:39 +0000

    Update models

--- backend/storia_18488/wsgi.py
+++ backend/storia_18488/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storia_18488.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storia_18488.settings")
 
 application = get_wsgi_application()
