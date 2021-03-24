9178a9ff8599ea37bb365c0ab19e32781a19fb40
Crowdbotics <team@crowdbotics.com>
2021-03-24 15:04:24 +0000

    remove package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -8,7 +8,6 @@ export const installed_blueprints = [
 { name: 'NotificationList72612', human_name: 'Notification List', access_route: 'NotificationList72612'},
 { name: 'Settings72611', human_name: 'Settings', access_route: 'Settings72611'},
 { name: 'Settings72603', human_name: 'Settings', access_route: 'Settings72603'},
-{ name: 'UserProfile72601', human_name: 'User Profile', access_route: 'UserProfile72601'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
 { name: 'Tutorial', human_name: 'Tutorial', access_route: 'Tutorial', icon: 'question-circle'},
 { name: 'Maps', human_name: 'Maps', access_route: 'Maps', icon: 'map'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -13,7 +13,6 @@ import Tutorial72640Navigator from '../features/Tutorial72640/navigator';
 import NotificationList72612Navigator from '../features/NotificationList72612/navigator';
 import Settings72611Navigator from '../features/Settings72611/navigator';
 import Settings72603Navigator from '../features/Settings72603/navigator';
-import UserProfile72601Navigator from '../features/UserProfile72601/navigator';
 import MessengerNavigator from '../features/Messenger/navigator';
 import TutorialNavigator from '../features/Tutorial/navigator';
 import MapsNavigator from '../features/Maps/navigator';
@@ -38,7 +37,6 @@ Tutorial72640: { screen: Tutorial72640Navigator },
 NotificationList72612: { screen: NotificationList72612Navigator },
 Settings72611: { screen: Settings72611Navigator },
 Settings72603: { screen: Settings72603Navigator },
-UserProfile72601: { screen: UserProfile72601Navigator },
 Messenger: { screen: MessengerNavigator },
 Tutorial: { screen: TutorialNavigator },
 Maps: { screen: MapsNavigator },
